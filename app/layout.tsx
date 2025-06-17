72f850a48f356f996e699bee554d52cd940e5de0
mHaines <mhaines9219@gmail.com>
2025-06-16 19:32:42 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import Footer from './components/Footer';
-import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Latter Band Chicago',
+  title: "Latter Band Chicago",
   description:
-    'LATTER is a Chicago-based experimental noise rock project formed by Jon Alvarado and Meredith Haines, known for dynamic live performances and innovative soundscapes.',
+    "LATTER is a Chicago-based experimental noise rock project formed by Jon Alvarado and Meredith Haines, known for dynamic live performances and innovative soundscapes.",
 };
 
 export default function RootLayout({
@@ -32,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
